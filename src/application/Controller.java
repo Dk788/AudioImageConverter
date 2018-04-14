72f095a82318e4d9b677d53f915b850670f68f22
zyw271828 <zyw_0618@outlook.com>
2018-04-14 19:12:57 +0800

    Implemented file drag and drop

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -2,15 +2,23 @@ package application;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.FileChooser;
@@ -19,8 +27,9 @@ import com.jfoenix.controls.JFXAlert;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.controls.JFXSpinner;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 
-public class Controller {
+public class Controller implements Initializable {
 	
 	private static File audioFile = null;
 	private static File imageFile = null;
@@ -68,6 +77,99 @@ public class Controller {
     @FXML
     private Label titleLabel;
 
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
+		transition.setRate(-1);
+		hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
+			transition.setRate(transition.getRate() * -1);
+			transition.play();
+			// TODO
+		});
+		
+		audioButton.setOnDragOver(new EventHandler<DragEvent>() {
+			@Override
+			public void handle(DragEvent event) {
+				if (event.getGestureSource() != audioButton && event.getDragboard().hasFiles()) {
+                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+                }
+                event.consume();
+			}
+		});
+		
+		audioButton.setOnDragDropped(new EventHandler<DragEvent>() {
+			@Override
+			public void handle(DragEvent event) {
+				boolean isSuccess = false;
+                if (event.getGestureSource() != audioButton && event.getDragboard().hasFiles()) {
+                    List<File> files = event.getDragboard().getFiles();
+                    if (files.size() != 1 || files.get(0).isDirectory()) {
+                    	showDialog("请拖入至多一个文件", false);
+					} else {
+						audioFile = files.get(0);
+						boolean isValid = false;
+						List<String> validExtensions = Arrays.asList("wav", "flac");
+						for (String ve : validExtensions) {
+							if (audioFile.getName().endsWith(ve)) {
+								isValid = true;
+							}
+						}
+						if (isValid) {
+							titleLabel.setText("音频图像转换器" + " - " + audioFile.getName());
+							imageButton.setDisable(true);
+							isSuccess = true;
+						} else {
+							showDialog("请拖入音频文件，支持 wav、flac 格式", false);
+						}
+					}
+                }
+                event.setDropCompleted(isSuccess);
+                event.consume();
+			}
+        });
+		
+		imageButton.setOnDragOver(new EventHandler<DragEvent>() {
+			@Override
+			public void handle(DragEvent event) {
+				if (event.getGestureSource() != imageButton && event.getDragboard().hasFiles()) {
+                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+                }
+                event.consume();
+			}
+		});
+		
+		imageButton.setOnDragDropped(new EventHandler<DragEvent>() {
+			@Override
+			public void handle(DragEvent event) {
+				boolean isSuccess = false;
+                if (event.getGestureSource() != imageButton && event.getDragboard().hasFiles()) {
+                    List<File> files = event.getDragboard().getFiles();
+                    if (files.size() != 1 || files.get(0).isDirectory()) {
+                    	showDialog("请拖入至多一个文件", false);
+					} else {
+						imageFile = files.get(0);
+						boolean isValid = false;
+						List<String> validExtensions = Arrays.asList("jpg", "png", "bmp");
+						for (String ve : validExtensions) {
+							if (imageFile.getName().endsWith(ve)) {
+								isValid = true;
+							}
+						}
+						if (isValid) {
+							titleLabel.setText("音频图像转换器" + " - " + imageFile.getName());
+							audioButton.setDisable(true);
+							isSuccess = true;
+						} else {
+							showDialog("请拖入图像文件，支持 jpg、png、bmp 格式", false);
+						}
+					}
+                }
+                event.setDropCompleted(isSuccess);
+                event.consume();
+			}
+        });
+	}
+    
     @FXML
     void onConvertBtnClick(ActionEvent event) {
     	if (audioFile == null && imageFile == null) {
@@ -75,6 +177,8 @@ public class Controller {
 		} else {
 			convertButton.setDisable(true);
 			clearButton.setDisable(true);
+			audioButton.setDisable(true);
+	    	imageButton.setDisable(true);
 	    	convertSpinner.setVisible(true);
 			if (audioFile != null) {
 				// audioToImage
@@ -89,6 +193,7 @@ public class Controller {
 							}
 							convertButton.setDisable(false);
 							clearButton.setDisable(false);
+							audioButton.setDisable(false);
 							audioFile = null;
 						});
 				    }
@@ -108,6 +213,7 @@ public class Controller {
 							}
 							convertButton.setDisable(false);
 							clearButton.setDisable(false);
+							imageButton.setDisable(false);
 							imageFile = null;
 						});
 				    }
@@ -154,11 +260,6 @@ public class Controller {
     		titleLabel.setText("音频图像转换器" + " - " + imageFile.getName());
 			audioButton.setDisable(true);
 		}
-    }
-    
-    @FXML
-    void onHbgClick(MouseEvent event) {
-    	// TODO
     }
     
     private boolean showDialog(String msg, boolean isShowCancelBtn) {
